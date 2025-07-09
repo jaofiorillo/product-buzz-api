e58df6aa1620a61e6456d2f194251a67f9ae2a3f
Fiorillo <joao.banci@xbrain.com.br>
2025-07-09 10:07:00 -0300

    increase token expiration time

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -13,7 +13,7 @@ import { UserModule } from 'src/user/user.module';
         JwtModule.register({
             global: true,
             secret: jwtConstants.secret,
-            signOptions: { expiresIn: '600s' },
+            signOptions: { expiresIn: '600000s' },
         }),
     ],
     providers: [{ provide: APP_GUARD, useClass: AuthGuard }, AuthService],
